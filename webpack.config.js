9c5b45ef43b9d1ee37e0111b676d4911622f2b78
Angel Mendoza <mendozaangel003@gmail.com>
2022-05-06 18:49:26 -0500

    installed dependencis to work

---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const path = require('path')
+const Dotenv = require('dotenv-webpack')
+const ESLintPlugin = require('eslint-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  mode: 'development',
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    alias: {
+      '@': path.resolve(__dirname, 'src/')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.(html)$/,
+        use: {
+          loader: 'html-loader'
+        }
+      },
+      {
+        test: /\.scss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        type: 'asset'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: 'index.html'
+    }),
+    new ESLintPlugin({
+      extensions: ['js', 'jsx']
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css'
+    }),
+    new Dotenv({
+      path: './.env',
+      systemvars: true
+    })
+  ],
+  devServer: {
+    historyApiFallback: true,
+    port: 3000
+  }
+}
