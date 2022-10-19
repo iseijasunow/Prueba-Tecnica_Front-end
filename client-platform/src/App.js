ade52d2604f59675c2f0b41291ddb0eb75578fcc
rbriceno <rbriceno@teravisiontech.com>
2022-10-18 20:29:51 -0500

    Layout and integration with the test api

--- client-platform/src/App.js
+++ client-platform/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/lib/integration/react";
+import TemplateRoutes from "./routes";
+import rootSaga from "./store/sagas";
+import configureStore from "./store";
+import "./stylesheet.scss";
+
+const { store, persistor, sagaMiddleware } = configureStore();
+sagaMiddleware.run(rootSaga);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <TemplateRoutes />
+        </PersistGate>
+      </Provider>
     </div>
   );
 }
