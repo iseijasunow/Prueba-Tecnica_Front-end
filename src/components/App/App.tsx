602e5ffa0be4ad1c879bee9a3264bf225f8c5454
eudaldmoya <eudald.moya.8@gmail.com>
2023-12-22 16:32:59 +0100

    Create routes in App component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,14 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import SearchPage from "../../pages/SearchPage/SearchPage";
+import paths from "../../paths/paths";
+
 function App() {
   return (
     <>
-      <h1>User app</h1>
+      <Routes>
+        <Route path={paths.search} element={<SearchPage />} />
+        <Route path={paths.root} element={<Navigate to={paths.search} />} />
+      </Routes>
     </>
   );
 }
---
+++ src/paths/paths.ts
@@ -0,0 +1,7 @@
+const paths = {
+  search: "/search",
+  root: "/",
+  detail: "/search/:userId",
+};
+
+export default paths;
