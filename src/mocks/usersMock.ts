a95f46777aeb72318328a86f6aa1bc7bd7ee4921
eudaldmoya <eudald.moya.8@gmail.com>
2023-12-22 19:15:43 +0100

    Set up MSW to intercept requests on tests

---
+++ src/mocks/usersMock.ts
@@ -0,0 +1,16 @@
+import { User } from "../types";
+
+export const usersMock: User[] = [
+  {
+    id: 1,
+    login: "user1",
+    avatar_url: "https://www.avatar.com",
+    followers_url: "https://www.followers.com",
+  },
+  {
+    id: 2,
+    login: "user2",
+    avatar_url: "https://www.avatar.com",
+    followers_url: "https://www.followers.com",
+  },
+];
