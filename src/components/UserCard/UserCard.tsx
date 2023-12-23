eb960688896f93bb0c1fcf0fc672d5cd1eb14bf5
eudaldmoya <eudald.moya.8@gmail.com>
2023-12-23 13:39:12 +0100

    Style UserCard component

--- src/components/UserCard/UserCard.tsx
+++ src/components/UserCard/UserCard.tsx
@@ -1,4 +1,5 @@
 import { User } from "../../types";
+import "./UserCard.scss";
 
 interface UserCardProps {
   user: User;
@@ -7,8 +8,14 @@ interface UserCardProps {
 const UserCard = ({ user: { id, login, avatar_url } }: UserCardProps) => {
   return (
     <article className="user-card">
-      <img src={avatar_url} alt={`${login} avatar`} />
-      <h2 className="user-card__name">{login}</h2>
+      <div>
+        <img
+          src={avatar_url}
+          alt={`${login} avatar`}
+          className="user-card__avatar"
+        />
+        <h2 className="user-card__name">{login}</h2>
+      </div>
       <span className="user-card__id">{id}</span>
     </article>
   );
