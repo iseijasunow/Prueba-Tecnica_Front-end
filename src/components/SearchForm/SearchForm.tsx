c8cb203460f997ef11e8b37ece8a2f714b7286fb
eudaldmoya <eudald.moya.8@gmail.com>
2023-12-23 16:33:43 +0100

    Add validators to form

--- src/components/SearchForm/SearchForm.tsx
+++ src/components/SearchForm/SearchForm.tsx
@@ -6,35 +6,63 @@ import UsersContext from "../../store/users/context/UsersContext";
 
 const SearchForm = () => {
   const [wordToSearch, setWordToSearch] = useState<string>("");
+  const [isShort, setIsShort] = useState<boolean>(true);
+  const [message, setMessage] = useState<string>("");
 
   const { loadUsers } = useContext(UsersContext);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setWordToSearch(event.target.value);
+
+    if (wordToSearch.length < 3) {
+      setIsShort(true);
+
+      return;
+    }
+
+    setIsShort(false);
   };
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
+    if (isShort) {
+      setMessage("Word must be 4 or more characters");
+      loadUsers("");
+
+      return;
+    }
+
+    if (wordToSearch.toLowerCase() === "iseijasunow") {
+      setMessage("You cannot search for iseijasunow");
+      loadUsers("");
+
+      return;
+    }
+
+    setMessage("");
     loadUsers(wordToSearch);
   };
 
   return (
-    <form className="form" onSubmit={handleSubmit}>
-      <label htmlFor="word" className="form__label">
-        User to search:{" "}
-      </label>
-      <input
-        type="text"
-        id="word"
-        placeholder="search"
-        className="form__input"
-        onChange={handleChange}
-      />
-      <button type="submit" className="form__button">
-        <FontAwesomeIcon icon={faSearch} color="#6f6f8b" size="2x" />
-      </button>
-    </form>
+    <>
+      <form className="form" onSubmit={handleSubmit}>
+        <label htmlFor="word" className="form__label">
+          User to search:{" "}
+        </label>
+        <input
+          type="text"
+          id="word"
+          placeholder="search"
+          className="form__input"
+          onChange={handleChange}
+        />
+        <button type="submit" className="form__button">
+          <FontAwesomeIcon icon={faSearch} color="#6f6f8b" size="2x" />
+        </button>
+      </form>
+      <span>{message}</span>
+    </>
   );
 };
 
