bb959d0ca385c679db42af766c1c06d02330ef78
eudaldmoya <eudald.moya.8@gmail.com>
2023-12-23 16:07:30 +0100

    Style UserList component

--- src/components/UserList/UserList.tsx
+++ src/components/UserList/UserList.tsx
@@ -1,12 +1,13 @@
 import { useContext } from "react";
 import UsersContext from "../../store/users/context/UsersContext";
 import UserCard from "../UserCard/UserCard";
+import "./UserList.scss";
 
 const UserList = () => {
   const { users } = useContext(UsersContext);
 
   return (
-    <ul>
+    <ul className="user-list">
       {users.map((user) => (
         <li key={user.id}>
           <UserCard user={user} />
