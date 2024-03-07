00945800ede1ac924f70f9b02af47812ff7d33f0
Pedro Gabriel <gaboleal123@gmail.com>
2024-03-07 17:15:38 -0400

    ajustar width bio

--- app-search-users-github/src/components/UserDetails/UserDetails.jsx
+++ app-search-users-github/src/components/UserDetails/UserDetails.jsx
@@ -6,7 +6,7 @@ function UserDetails({ selectedUser }) {
     <div>
       <h2>{selectedUser.name}</h2>
       <img src={selectedUser.avatar_url} alt={selectedUser.login} />
-      <p>{selectedUser.bio}</p>
+      <p className="bio">{selectedUser.bio}</p>
       <div className="user-stats">
         <p>Followers: {selectedUser.followers}</p>
         <p>Following: {selectedUser.following}</p>
