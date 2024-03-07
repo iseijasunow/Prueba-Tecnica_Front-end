fe54f1f339cc3a07a81175c739a2b02fd4577754
Pedro Gabriel <gaboleal123@gmail.com>
2024-03-07 15:35:50 -0400

    validacion errores

--- app-search-users-github/src/pages/HomePage/HomePage.jsx
+++ app-search-users-github/src/pages/HomePage/HomePage.jsx
@@ -6,15 +6,27 @@ import { getUsers } from '../../services/githubService';
 function HomePage() {
   const [searchTerm, setSearchTerm] = useState('');
   const [users, setUsers] = useState([]);
-  
+  const [error, setError] = useState('');
 
   const handleSearch = async () => {
-    try {
-      const response = await getUsers(searchTerm);
-      setUsers(response.data.items.slice(0, 10));
-    } catch (error) {
-      console.error('Error fetching users:', error);
-    }
+      if (searchTerm.length < 4) {
+        setError('El término de búsqueda debe tener al menos 4 caracteres.');
+        return;
+      }
+
+      if (searchTerm.toLowerCase() === 'iseijasunow') {
+        setError('La búsqueda de "iseijasunow" no está permitida.');
+        return;
+      }
+
+      try {
+        setError('');
+        const response = await getUsers(searchTerm);
+        setUsers(response.data.items.slice(0, 10));
+      } catch (error) {
+        console.error('Error fetching users:', error);
+        setError('Error al buscar usuarios. Por favor, inténtalo de nuevo más tarde.');
+      }
   };
 
 
@@ -22,6 +34,7 @@ function HomePage() {
     <div>
       <h1>Buscador de Usuario Github</h1>
       <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} handleSearch={handleSearch} />
+      {error && <div style={{ color: 'red' }}>{error}</div>}
       <UserList users={users} />
     </div>
   );
