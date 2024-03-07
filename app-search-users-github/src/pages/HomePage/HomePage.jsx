5abea57e45aad5eac616ac22e03cf84fdf392ae7
Pedro Gabriel <gaboleal123@gmail.com>
2024-03-07 16:11:04 -0400

    graficos

--- app-search-users-github/src/pages/HomePage/HomePage.jsx
+++ app-search-users-github/src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import Chart from 'chart.js/auto';
+
 import SearchBar from '../../components/SearchBar/SearchBar';
 import UserList from '../../components/UserList/UserList';
 import { getUsers } from '../../services/githubService';
+import axios from 'axios';
 
 function HomePage() {
   const [searchTerm, setSearchTerm] = useState('');
   const [users, setUsers] = useState([]);
   const [error, setError] = useState('');
+  const [followersData, setFollowersData] = useState(null);
+  const chartRef = useRef(null);
+
+  useEffect(() => {
+
+    if (users.length > 0) {
+      generateFollowersData();
+    }
+  }, [users]);
 
   const handleSearch = async () => {
       if (searchTerm.length < 4) {
@@ -22,11 +34,50 @@ function HomePage() {
       try {
         setError('');
         const response = await getUsers(searchTerm);
-        setUsers(response.data.items.slice(0, 10));
+        const userData = response.data.items.slice(0, 10);
+        const usersWithFollowers = await Promise.all(userData.map(async (user) => {
+          const userDetails = await axios.get(user.url); // Obtener detalles del usuario
+          return { ...user, followers: userDetails.data.followers }; // Agregar el total de seguidores al objeto del usuario
+        }));
+        setUsers(usersWithFollowers);
       } catch (error) {
         console.error('Error fetching users:', error);
         setError('Error al buscar usuarios. Por favor, inténtalo de nuevo más tarde.');
       }
+  };
+
+  const generateFollowersData = () => {
+
+    setFollowersData(users.map(user => user.followers));
+    const labels = users.map(user => user.login);
+
+    const ctx = document.getElementById('followersChart');
+
+    if (chartRef.current) {
+      chartRef.current.destroy();
+    }
+
+    chartRef.current = new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: labels,
+        datasets: [{
+          label: 'Followers',
+          data: followersData,
+          backgroundColor: 'rgba(75, 192, 192, 0.2)',
+          borderColor: 'rgba(75, 192, 192, 1)',
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+    
   };
 
 
@@ -36,6 +87,9 @@ function HomePage() {
       <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} handleSearch={handleSearch} />
       {error && <div style={{ color: 'red' }}>{error}</div>}
       <UserList users={users} />
+      <div>
+        {<canvas id="followersChart" width="400" height="200"></canvas>}
+      </div>
     </div>
   );
 }
