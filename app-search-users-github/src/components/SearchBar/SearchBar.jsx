7c24311ca76c82085e7747e3d76a43a34ee7b4f1
Pedro Gabriel <gaboleal123@gmail.com>
2024-03-08 14:33:45 -0400

    Update SearchBar.jsx

--- app-search-users-github/src/components/SearchBar/SearchBar.jsx
+++ app-search-users-github/src/components/SearchBar/SearchBar.jsx
@@ -6,7 +6,7 @@ function SearchBar({ searchTerm, setSearchTerm, handleSearch }) {
   return (
     <div className="search-container">
        <h3>Buscador de Usuario Github</h3>
-      <input type="text" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
+      <input type="text" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} placeholder='Ingresar nombre'/>
       <button onClick={handleSearch}> <FontAwesomeIcon icon={faSearch} /></button>
     </div>
   );
