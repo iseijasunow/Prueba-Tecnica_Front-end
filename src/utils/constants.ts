a193f0c0d2d0f5cb6b5753622106508534e759c7
YoimelDev <yoimeldev@gmail.com>
2024-03-17 16:53:28 -0400

    feat: :sparkles: Add useGetUser and useSearchUsers hooks

---
+++ src/utils/constants.ts
@@ -0,0 +1,11 @@
+export const GITHUB_API_URL = 'https://api.github.com';
+
+export const SEARCH_USERS_ENDPOINT = '/search/users';
+export const RESULTS_PER_PAGE = 10;
+
+export const USER_ENDPOINT = '/users';
+
+export const ERROR_FETCHING_USERS_DATA = 'Ocurrió un error al obtener los datos de los usuarios';
+export const ERROR_FETCHING_USER_DATA = 'Ocurrió un error al obtener los datos del usuario';
+export const ERROR_INVALID_USERNAME = 'El nombre de usuario "iseijasunow" no está permitido';
+export const ERROR_USERNAME_TOO_SHORT = 'El nombre de usuario debe tener al menos 4 caracteres';
